201781e3f3ac92415277757609de82b9a60fe184
hanaerachid <110319135+hanaerachid@users.noreply.github.com>
2023-08-06 08:56:26 +0000

    Pending changes exported from your codespace

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styles from './content.module.scss'
+import { Button, Form, TextInput, Tile } from 'carbon-components-react'
+import { Heading, Section } from 'carbon-components-react/lib/components/Heading';
+import { Add20 } from '@carbon/icons-react';
+
+interface RegistrationFormState {
+  name: string;
+  email: string;
+  tel: string;
+}
+
+const initialFormState: RegistrationFormState = {
+  name: '',
+  email: '',
+  tel: ''
+};
+
 
 const Content = (): JSX.Element => {
-  return <h5 className={styles.content}>This is simple React app</h5>
+
+  const [formState, setFormState] = useState<RegistrationFormState>(initialFormState);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormState(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log(formState);
+    // Reset form state
+    setFormState(initialFormState);
+  };
+
+
+  return (
+
+    <>
+      <br />
+      <br />
+      <br />
+      <div className={styles.content}>
+        <Tile>
+          <Section as="div">
+            <Heading>Register now</Heading>
+            <br />
+            <Form onSubmit={handleSubmit} >
+              <TextInput value={formState.name}
+                onChange={handleChange} name="name" type="text" size="lg" placeholder="John Doe" id={'edwefe'} labelText={'Name'} />
+              <br />
+              <TextInput value={formState.tel}
+                onChange={handleChange} name="telephone" type="tel" size="lg" placeholder="+1555332323" id={'edwefe'} labelText={'Phone Number'} />
+              <br />
+              <TextInput value={formState.email}
+                onChange={handleChange} name="email" type="email" size="lg" placeholder="john@doe.org" id={'edwefe'} labelText={'Email'} />
+              <br />
+              <Button renderIcon={Add20} type="submit">Submit</Button>
+            </Form>
+          </Section>
+        </Tile>
+      </div>
+    </>)
 }
 
 export default Content
--- src/components/header/CarbonHeader.tsx
+++ src/components/header/CarbonHeader.tsx
@@ -1,31 +1,18 @@
 import React from 'react'
 import {
   Header,
-  SkipToContent,
   HeaderName,
-  HeaderGlobalBar,
-  HeaderGlobalAction,
+  SkipToContent,
 } from 'carbon-components-react'
-import { Search20, Notification20, Switcher20 } from '@carbon/icons-react'
 
 const CarbonHeader = (): JSX.Element => {
   return (
     <Header aria-label="IBM Platform Name">
       <SkipToContent />
-      <HeaderName href="#" prefix="IBM">
-        [Platform]
+      <HeaderName href="#" prefix="GEOAP">
+        Training Registration
       </HeaderName>
-      <HeaderGlobalBar>
-        <HeaderGlobalAction aria-label="Search">
-          <Search20 />
-        </HeaderGlobalAction>
-        <HeaderGlobalAction aria-label="Search">
-          <Notification20 />
-        </HeaderGlobalAction>
-        <HeaderGlobalAction aria-label="Search">
-          <Switcher20 />
-        </HeaderGlobalAction>
-      </HeaderGlobalBar>
+
     </Header>
   )
 }
